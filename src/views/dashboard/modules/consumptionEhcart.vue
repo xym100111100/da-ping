29e88959c70514b0d546532583fa5b2c2a56b2d9
kcz <1150791676@qq.com>
2021-05-29 00:06:12 +0800

    feat: 新增消费人群各项指标汇总 map 图

--- src/views/dashboard/modules/consumptionEhcart.vue
+++ src/views/dashboard/modules/consumptionEhcart.vue
@@ -22,9 +22,6 @@ export default {
     await this.initAmap()
     this.$nextTick(() => {
       this.initChart()
-      this.loadAreaNode(100000).then(res => {
-        console.log(res)
-      })
     })
   },
   methods: {
@@ -53,13 +50,13 @@ export default {
       return new Promise((resolve, reject) => {
         try {
           window.AMapUI.load(
-            ['ui/geo/DistrictExplorer', 'lib/$'],
-            (DistrictExplorer, $) => {
+            ['ui/geo/DistrictExplorer'],
+            (DistrictExplorer) => {
               // 创建一个实例
               this.districtExplorer = (window.districtExplorer =
-            new DistrictExplorer({
-              eventSupport: true // 打开事件支持
-            }))
+                new DistrictExplorer({
+                  eventSupport: true // 打开事件支持
+                }))
               resolve()
             }
           )
@@ -73,36 +70,75 @@ export default {
      * @param {*}
      * @return {*}
      */
-    initChart() {
+    async initChart() {
+      const AreaNode = await this.loadAreaNode(440000)
+      const mapName = AreaNode.getName()
+      const geoJson = {}
+      geoJson.features = AreaNode.getSubFeatures()
+      geoJson.type = 'FeatureCollection'
+
       this.chart = echarts.init(this.$refs.main)
+
+      echarts.registerMap(mapName, geoJson)
       // 指定图表的配置项和数据
       var option = {
-        color: ['#5bcbff', '#52d9f5', '#7ea4fd', '#63e6f0'],
-        tooltip: {},
-        legend: {
-          data: ['大米', '大豆', '食用油', '面粉']
+
+        tooltip: {
+          trigger: 'item',
+          formatter: `
+          {b}
+          <br/>
+          客户数 2315人
+          <br/>
+          交易数 452笔
+          <br/>
+          交易额 4854575元
+          <br/>
+          客户浓度 56%
+          `
         },
-        xAxis: {
-          data: ['广州市', '广州市', '广州市', '广州市', '广州2市', '广州市']
+
+        visualMap: {
+          min: 800,
+          max: 50000,
+          text: ['High', 'Low'],
+          realtime: false,
+          calculable: true,
+          inRange: {
+            color: ['lightskyblue', 'yellow', 'orangered']
+          }
         },
-        yAxis: {},
-        series: [{
-          name: '大米',
-          type: 'bar',
-          data: [3000, 20, 4, 235, 235, 234]
-        }, {
-          name: '大豆',
-          type: 'bar',
-          data: [5, 233, 36, 235, 235, 20]
-        }, {
-          name: '食用油',
-          type: 'bar',
-          data: [5, 20, 234, 5235, 234, 523]
-        }, {
-          name: '面粉',
-          type: 'bar',
-          data: [5, 20, 235, 12350, 6236, 234]
-        }]
+        series: [
+          {
+            name: '数据展示',
+            type: 'map',
+            mapType: mapName, // 自定义扩展图表类型
+            label: {
+              show: true
+            },
+            data: [
+              { name: '肇庆市', value: 20057.34 },
+              { name: '清远市', value: 15477.48 },
+              { name: '韶关市', value: 31686.1 },
+              { name: '广州市', value: 6992.6 },
+              { name: '油尖旺', value: 44045.49 },
+              { name: '深水埗', value: 40689.64 },
+              { name: '九龙城', value: 37659.78 },
+              { name: '黄大仙', value: 45180.97 },
+              { name: '观塘', value: 55204.26 },
+              { name: '葵青', value: 21900.9 },
+              { name: '荃湾', value: 4918.26 },
+              { name: '屯门', value: 5881.84 },
+              { name: '元朗', value: 4178.01 },
+              { name: '北区', value: 2227.92 },
+              { name: '大埔', value: 2180.98 },
+              { name: '沙田', value: 9172.94 },
+              { name: '西贡', value: 3368 },
+              { name: '离岛', value: 806.98 }
+            ]
+
+          }
+        ]
       }
 
       // 使用刚指定的配置项和数据显示图表。
@@ -112,7 +148,7 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-  .chart-box{
-    height: 578px;
-  }
+.chart-box {
+  height: 578px;
+}
 </style>
